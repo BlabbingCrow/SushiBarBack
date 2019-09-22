a3166572ef4bdfc495ef709b17559f0c2b7ac904
Данил Аникьев <anikyev95@mail.ru>
2019-09-22 14:59:03 +0400

    tests

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+// содежимое index.js
+const http = require('http');
+const port = 3000;
+const requestHandler = (request, response) => {
+    console.log(request.url);
+    response.end('Hello Node.js Server!');
+}
+const server = http.createServer(requestHandler);
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err);
+    }
+    console.log(`server is listening on ${port}`);
+})
